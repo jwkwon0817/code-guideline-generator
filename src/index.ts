ddff66845647b278795224369e30bb08f530deab
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-04-27 02:29:44 +0900

    feat: implement CLI for generating Biome guideline configuration

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 #!/usr/bin/env node
 
+import { Command } from 'commander';
 import { installDependencies } from "./utils/install";
 import Logger from "./utils/logger";
 import { getDefaultConfiguration } from "./utils/metadata";
 import { askIndentStyle, askIndentWidth, askQuoteStyles, askSemicolon, checkIfUsingGit, Wizard } from "./utils/wizard";
 
+const program = new Command();
+
 async function main() {
   await installDependencies();
-  
+
   const result = await Wizard.pipeline(
     getDefaultConfiguration(), 
     [
@@ -17,12 +20,22 @@ async function main() {
       askQuoteStyles,
       askSemicolon,
     ]
-  )
+  );
 
   await Wizard.generate(result);
+  Logger.success('Code Guideline successfully generated!');
 }
 
-main()
-  .then(() => {
-    Logger.success('Code Guideline successfully generated!');
-  })
+program
+  .name('code-guideline')
+  .description('CLI to generate a Biome guideline config')
+  .version('1.0.0');
+
+program
+  .command('init')
+  .description('Initialize a new Biome config')
+  .action(async () => {
+    await main();
+  });
+
+program.parse();
